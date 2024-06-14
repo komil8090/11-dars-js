2e4ed0fa80434983c214e80a5ee39d3e55c221b3
Mirkomil Mirvohidov <mirkomilmirvohidov4@gmail.com>
2024-06-14 18:42:35 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,70 @@
+// 1 - vazifa
+// let son1 = Number(prompt("son1"));
+// let son2 = Number(prompt("son2"));
+
+// alert(`${son1} + ${son2} = ${son1 + son2}`);
+
+
+
+// 2 - vazifa
+// let a = "12";
+// let b = Number(a);
+
+// alert(b);
+
+// 3 - vazifa
+// let tahminison = Math.floor(Math.random() * 1000);
+
+// if (tahminison % 2 === 0) {
+//     console.log(`${tahminison} -> juft son`); 
+// } else {
+//     console.log(`${tahminison} -> tog' son`)
+// }
+
+// let mars = prompt("soz kriting")
+
+// if (mars === "Mars") {
+//     console.log(`${mars} -> it School`)
+// }
+
+// let narsalar = ["Ranaldo", "Mers", "Asus", "Apple", "Alfajr"];
+// let ta5 = [];
+// let ta4 = [];
+
+// for (let narsa of narsalar) {
+//   if (narsa.length > 5) {
+//     ta5.push(narsa);
+//   } else {
+//     ta4.push(narsa)
+//   }
+// }
+
+// console.log(ta4);
+// console.log(ta5);
+
+
+
+
+// let sonlar = [2, 4, 9, 10, 45, 67, 8, 90];
+// let juftlar = [];
+// let toqlar = [];
+
+// for (let i = 0; i < 8; i++) {
+//   if (sonlar[i] % 2 === 0) {
+//     juftlar.push(sonlar[i]);
+//   } else {
+//     toqlar.push(sonlar[i]);
+//   }
+// }
+
+// console.log(juftlar);
+// console.log(toqlar);
+
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 2 === 0) {
+      console.log(${i} -> juft son)
+  } else {
+      console.log(${i} -> toq son)
+  }
+}
